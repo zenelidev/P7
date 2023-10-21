53a6182d91a398fbafc5c6fd61eae0c62c497264
Sarah Zeneli <sarahlea1998@gmail.com>
2023-10-21 16:08:52 +0200

    update

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,30 @@ const router = require('./routes/index')
 const path = require('path');
 
 const app = express();
+const cors = require("cors");
+app.use(
+  cors({
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+    optionsSuccessStatus: 200,
+    origin: "http://localhost:3000",
+  })
+);
+
+// Définition des en-têtes CORS de base pour les requêtes entrantes
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
 app.use(express.json());
 
--- backend/server.js
+++ backend/server.js
@@ -35,12 +35,6 @@ const errorHandler = error => {
   }
 };
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
 
 const server = http.createServer(app);
 
